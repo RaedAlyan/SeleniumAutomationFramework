633769a25142fafe327ca4d58653f390121ec7cf
Raed Eleyan <raedmalyan@gmail.com>
2024-12-06 10:38:39 +0200

    added some enhancements to the config_reader module

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -1,22 +1,27 @@
 import json
+from pathlib import Path
 from logging import getLogger
 
 logger = getLogger(__name__)
 
 
-def load_config(config_file='config_files/config.json') -> dict:
+def load_config(config_file : str = 'config_files/config.json') -> dict:
     """
      Loads and returns the configuration from a JSON file.
 
     :return: A dictionary containing the configuration data.
     :raises FileNotFoundError: If the configuration file does not exist.
     """
-    logger.info(f'********** {load_config.__name__} **********')
+    logger.info(f'********** {load_config.__name__}() **********')
     try:
-        logger.info(f'Loading configuration from: {config_file}')
-        with open(config_file) as f:
+        current_dir = Path(__file__).parent
+        config_path = (current_dir / ".." / config_file).resolve()
+        logger.info(f'Loading configuration from: {config_path}')
+        with open(config_path) as f:
             config = json.load(f)
         logger.info('The configuration file loaded successfully.')
         return config
     except FileNotFoundError as e:
         logger.error(f'The configuration file was not found. Error: {e}')
+    except Exception as e:
+        logger.error(f'An error occurred while loading the configuration file. Error: {e}')
