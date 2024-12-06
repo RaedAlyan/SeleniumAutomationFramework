8a4e6006774b6591d8646cb363f30b4bd8d9c974
Raed Eleyan <raedmalyan@gmail.com>
2024-12-06 13:52:35 +0200

    added some enhancements and new methods to the random_data_generator.py

--- utils/random_data_generator.py
+++ utils/random_data_generator.py
@@ -3,15 +3,38 @@ from faker import Faker
 
 class RandomDataGenerator:
 
-    def __init__(self):
-        self.faker = Faker()
+    def __init__(self, locale: str = 'en_US'):
+        self.faker = Faker(locale)
 
-    def get_random_name(self):
+    def generate_random_first_name(self) -> str:
+        """Generates a random first name."""
+        return self.faker.first_name()
+
+    def generate_random_last_name(self) -> str:
+        """Generates a random last name."""
+        return self.faker.last_name()
+
+    def generate_random_full_name(self) -> str:
+        """Generates a random full name."""
         return self.faker.name()
 
-    def get_random_email(self):
+    def generate_random_email(self) -> str:
+        """Generates a random email address."""
         return self.faker.email()
 
-    def get_random_phone_number(self):
+    def generate_random_phone_number(self) -> str:
+        """Generates a random phone number."""
         return self.faker.phone_number()
 
+    def generate_random_password(self, length: int = 12, special_chars: bool = True, upper_case: bool = True,
+                                 digits: bool = True) -> str:
+        """
+        Generates a random password with customizable complexity.
+
+        :param length: Length of the password (default is 12).
+        :param special_chars: Whether to include special characters (default is True).
+        :param upper_case: Whether to include uppercase letters (default is True).
+        :param digits: Whether to include digits (default is True).
+        :return: A randomly generated password.
+        """
+        return self.faker.password(length=length, special_chars=special_chars, upper_case=upper_case, digits=digits)
