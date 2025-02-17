f411b2bf7a86746cc8b098d39427b474b3ec70ca
Raed Eleyan <raedeleyan1@gmail.com>
2025-02-17 23:51:01 +0200

    Implemented the webdriver_initializer.py module to initialize a WebDriver based on the specified browser within the config.json file

---
+++ utils/webdriver_initializer.py
@@ -0,0 +1,48 @@
+"""
+This module provides a class to initialize and manage WebDriver instances for different browsers.
+
+@author: Raed Eleyan.
+@date: 02/17/2025.
+@contact: raedeleyan1@gmail.com.
+"""
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from config_loader import ConfigLoader
+
+
+class WebDriverInitializer:
+
+    def __init__(self) -> None:
+        """
+        Initializes the WebDriverInitializer by loading the browser configuration.
+        """
+        self.config = ConfigLoader()
+        self.browser = self.config.get_specified_browser().lower()
+
+    def initialize_webdriver(self) -> WebDriver:
+        """
+        Initializes and returns a WebDriver instance for the specified browser.
+
+        :return: an instance of the WebDriver for the specified browser.
+        :raises KeyError: If the specified browser is not supported.
+        :raises Exception: if an error occurred while initializing the WebDriver.
+        """
+        try:
+            if self.browser == 'chrome':
+                web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            elif self.browser == 'firefox':
+                web_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+            elif self.browser == 'edge':
+                web_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+            else:
+                raise KeyError(f'The browser {self.browser} is not supported.')
+            return web_driver
+        except Exception as e:
+            raise Exception(f'An error occurred while initializing the WebDriver. The error: {e}')
+
