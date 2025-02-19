7b8b1cf44372faa0ae7bc73c4b8ee40fc76e7017
Raed Eleyan <raedeleyan1@gmail.com>
2025-02-19 19:49:23 +0200

    Implemented some WebDriver methods to handle and interact with dropdown web_element

--- pages/base_page.py
+++ pages/base_page.py
@@ -165,3 +165,166 @@ class BasePage:
             self.driver.close()
         except Exception as e:
             raise Exception(f'An error occurred while closing the current window. Error: {e}')
+
+    def is_dropdown_multiple_selections(self, locator: tuple) -> bool:
+        """
+        Checks if the dropdown supports multiple selections.
+
+        :param locator: the locator strategy and value.
+        :return: True if the dropdown supports multiple selections, otherwise False.
+        :raises Exception: if an error occurs while checking if the dropdown supports multiple selections.
+        """
+        try:
+            web_element = self.find_element(locator)
+            dropdown = Select(web_element)
+            return dropdown.is_multiple
+        except Exception as e:
+            raise Exception(f'An error occurred while checking the dropdown supports multiple selections. Error: {e}')
+
+    def select_dropdown_by_visible_text(self, locator: tuple, text: str) -> None:
+        """
+        Selects a dropdown option by a visible text.
+
+        :param locator: the locator strategy and value.
+        :param text: the visible text of the option to select.
+        :raises Exception: if an error occurs while selecting a dropdown option by a visible text.
+        """
+        try:
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.select_by_visible_text(text)
+        except Exception as e:
+            raise Exception(f'An error occurred while selecting a dropdown option by a visible text. Error: {e}')
+
+    def select_dropdown_by_value(self, locator: tuple, value: str) -> None:
+        """
+        Selects a dropdown option by its value attribute.
+
+        :param locator: the locator strategy and value.
+        :param value: the value attribute of the option to select.
+        :raises Exception: if an error occurs while selecting a dropdown option by a value attribute.
+        """
+        try:
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.select_by_value(value)
+        except Exception as e:
+            raise Exception(f'An error occurred while selecting a dropdown option by a value. Error: {e}')
+
+    def select_dropdown_by_index(self, locator: tuple, index: int) -> None:
+        """
+        Selects a dropdown option by its index.
+
+        :param locator: the locator strategy and value.
+        :param index: the index of the option to select.
+        :raises Exception: if an error occurs while selecting a dropdown option by an index.
+        """
+        try:
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.select_by_index(index)
+        except Exception as e:
+            raise Exception(f'An error occurred while selecting a dropdown option by an index. Error: {e}')
+
+    def get_all_dropdown_options(self, locator: tuple) -> list:
+        """
+        Returns all options in a dropdown as a list of strings.
+
+        :param locator: the locator strategy and value.
+        :return: a list of all dropdown options.
+        :raises Exception: if an error occurs while getting all dropdown options.
+        """
+        try:
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            options = [option.text for option in drop_down.options]
+            return options
+        except Exception as e:
+            raise Exception(f'An error occurred while getting all dropdown options. Error: {e}')
+
+    def get_selected_dropdown_option(self, locator: tuple) -> str:
+        """
+        Returns the currently selected option in a dropdown.
+
+        :param locator: the locator strategy and value.
+        :return: the text of the currently selected option.
+        :raises Exception: if an error occurs while getting the currently selected dropdown option.
+        """
+        try:
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            return drop_down.first_selected_option.text
+        except Exception as e:
+            raise Exception(f'An error occurred while getting currently selected dropdown option. Error: {e}')
+
+    def deselect_all_dropdown_options(self, locator: tuple) -> None:
+        """
+        Deselects all selected options in a multi-select dropdown.
+
+        :param locator: the locator strategy and value.
+        :raises InvalidArgumentException: If the dropdown is not a multi-select dropdown.
+        :raises Exception: if an error occurs while deselecting all dropdown options.
+        """
+        try:
+            if not self.is_dropdown_multiple_selections(locator):
+                raise InvalidArgumentException('The dropdown is not a multi-select dropdown.')
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.deselect_all()
+        except Exception as e:
+            raise Exception(f'An error occurred while deselecting all dropdown options. Error: {e}')
+
+    def deselect_dropdown_by_index(self, locator: tuple, index: int) -> None:
+        """
+        Deselects a dropdown option by its index in a multi-select dropdown.
+
+        :param locator: the locator strategy and value.
+        :param index: the index of the option to deselect.
+        :raises InvalidArgumentException: If the dropdown is not a multi-select dropdown.
+        :raises Exception: if an error occurs while deselecting a dropdown option by an index.
+        """
+        try:
+            if not self.is_dropdown_multiple_selections(locator):
+                raise InvalidArgumentException('The dropdown is not a multi-select dropdown.')
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.deselect_by_index(index)
+        except Exception as e:
+            raise Exception(f'An error occurred while deselecting a dropdown option by an index. Error: {e}')
+
+    def deselect_dropdown_by_value(self, locator: tuple, value: str) -> None:
+        """
+        Deselects a dropdown option by its value attribute in a multi-select dropdown.
+
+        :param locator: the locator strategy and value.
+        :param value: the value attribute of the option to deselect.
+        :raises InvalidArgumentException: If the dropdown is not a multi-select dropdown.
+        :raises Exception: if an error occurs while deselecting a dropdown option by a value.
+        """
+        try:
+            if not self.is_dropdown_multiple_selections(locator):
+                raise InvalidArgumentException('The dropdown is not a multi-select dropdown.')
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.deselect_by_value(value)
+        except Exception as e:
+            raise Exception(f'An error occurred while deselecting a dropdown option by a value. Error: {e}')
+
+    def deselect_dropdown_by_visible_text(self, locator: tuple, text: str) -> None:
+        """
+        Deselects a dropdown option by its visible text in a multi-select dropdown.
+
+        :param locator: the locator strategy and value.
+        :param text: the visible text of the option to deselect.
+        :raises InvalidArgumentException: If the dropdown is not a multi-select dropdown.
+        :raises Exception: if an error occurs while selecting a dropdown option by a visible text.
+        """
+        try:
+            if not self.is_dropdown_multiple_selections(locator):
+                raise InvalidArgumentException('The dropdown is not a multi-select dropdown.')
+            web_element = self.find_element(locator)
+            drop_down = Select(web_element)
+            drop_down.deselect_by_visible_text(text)
+        except Exception as e:
+            raise Exception(f'An error occurred while selecting a dropdown option by a visible text. Error: {e}')
+
