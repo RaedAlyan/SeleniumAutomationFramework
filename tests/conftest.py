851908d481668b057c86b43165dd0345608637ba
Raed Eleyan <raedeleyan1@gmail.com>
2025-03-02 16:44:52 +0200

    Updated the conftest.py module by adding logging functionalities to it

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,29 @@ This module defines shared test fixtures for Pytest.
 """
 import pytest
 from ..utils.webdriver_initializer import WebDriverInitializer
+from ..utils.logger import Logger
 from selenium.common.exceptions import WebDriverException
 
+
+logger = Logger()
 
 @pytest.fixture(scope='session')
 def driver():
     """Fixture to initialize and yield a WebDriver instance."""
+    logger.log_method_entry('The Driver Fixture')
     webdriver = None
     try:
+        logger.info('Initializing WebDriver...')
         webdriver_initializer = WebDriverInitializer()
         webdriver = webdriver_initializer.initialize_webdriver()
+        logger.info('WebDriver initialized successfully.')
         yield webdriver
     except WebDriverException as e:
+        logger.error('Failed to initialize the WebDriver.')
         raise WebDriverException(f'An error occurred while trying to initialize the webdriver. Error: {e}')
     finally:
         if webdriver is not None:
+            logger.info('Quitting WebDriver...')
             webdriver.quit()
+            logger.info('WebDriver quit successfully.')
 
