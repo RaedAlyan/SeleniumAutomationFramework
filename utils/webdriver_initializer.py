713316723dc6b13b35ace954589f7901cdef44b6
Raed Eleyan <raedeleyan1@gmail.com>
2025-03-03 02:18:20 +0200

    Updated the webdriver_initializer.py module to support adding browser options

--- utils/webdriver_initializer.py
+++ utils/webdriver_initializer.py
@@ -18,6 +18,10 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from config_loader import ConfigLoader
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from typing import Union
 
 
 class WebDriverInitializer:
@@ -29,6 +33,33 @@ class WebDriverInitializer:
         self.config = ConfigLoader()
         self.browser = self.config.get_specified_browser().lower()
 
+    def _get_browser_options(self) -> Union[ChromeOptions, FirefoxOptions, EdgeOptions]:
+        """
+        Creates and returns browser-specific options based on the specified browser in the config.json file.
+
+        :return: An instance of browser-specific options (e.g., ChromeOptions, FirefoxOptions, EdgeOptions).
+        :raises KeyError: If the specified browser is not supported.
+        """
+        try:
+            browser_options = self.config.get_browser_options()[self.browser]
+            if self.browser == "chrome":
+                options = ChromeOptions()
+            elif self.browser == "firefox":
+                options = FirefoxOptions()
+            elif self.browser == "edge":
+                options = EdgeOptions()
+            elif self.browser == 'chromium':
+                options = ChromeOptions()
+            elif self.browser == 'brave':
+                options = ChromeOptions()
+            else:
+                raise KeyError(f'The browser {self.browser} is not supported.')
+            for option in browser_options:
+                options.add_argument(option)
+            return options
+        except KeyError as e:
+            raise KeyError(f'The browser_options option wasn\'t found in the config.json file. Error: {e}')
+
     def initialize_webdriver(self) -> WebDriver:
         """
         Initializes and returns a WebDriver instance for the specified browser.
@@ -38,18 +69,19 @@ class WebDriverInitializer:
         :raises WebDriverException: when an error occurred while trying to initialize the WebDriver.
         """
         try:
+            options = self._get_browser_options()
             if self.browser == 'chrome':
-                web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+                web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
             elif self.browser == 'firefox':
-                web_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+                web_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
             elif self.browser == 'edge':
-                web_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+                web_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
             elif self.browser == 'chromium':
                 web_driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(
-                    chrome_type=ChromeType.CHROMIUM).install()))
+                    chrome_type=ChromeType.CHROMIUM).install()), options=options)
             elif self.browser == 'brave':
                 web_driver = webdriver.Chrome(service=BraveService(ChromeDriverManager(
-                    chrome_type=ChromeType.BRAVE).install()))
+                    chrome_type=ChromeType.BRAVE).install()), options=options)
             else:
                 raise KeyError(f'The browser {self.browser} is not supported.')
             return web_driver
